c4445ffc5aa008d17043473271c3fbf8a57a7d92
H <f@gmail.com>
2024-01-08 15:41:29 +0700

    complete

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -3,7 +3,28 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+	for (int i = 1; i < N; i++)
+	{
+		T temp = d[i];
+		for (int j = i - 1; j >= 0; j--)
+		{
+			if (d[j] < temp)
+			{
+				d[j + 1] = d[j];
+				if (j == 0)
+					d[j] = temp;
+			}
+			else
+			{
+				d[j + 1] = temp;
+				break;
+			}
+		}
+		cout << "Pass " << i << ":";
+		for (int k = 0; k < 10; k++)
+			cout << d[k] << " ";
+		cout << endl;
+	}
 }
 
 int main(){
